2f3ec81f24a11f861bc8a220b29c970cc4d4ef92
Geoff Hing <geoffhing@gmail.com>
2016-04-08 11:53:34 -0500

    Add Cook private schools and some non-Cook public schools
    Addresses https://github.com/ghing/learning-collaborative-maps/issues/23
    and https://github.com/ghing/learning-collaborative-maps/issues/21

--- scripts/filter_schools.js
+++ scripts/filter_schools.js
@@ -3,7 +3,7 @@
 var stringify = require('csv-stringify');
 var xlsx = require('xlsx');
 
-var WORKSHEET_NAME = 'Public Dist & Sch';
+var SOURCE_WOORKSHEETS = new Set(['Public Dist & Sch', 'Non Pub Sch']);
 
 var COUNTY_NAME_COLUMN_INDEX = 0;
 var REC_TYPE_COLUMN_INDEX = 1;
@@ -27,29 +27,56 @@ var OUTPUT_COLUMNS = [
   GRADE_SERVED_COLUMN_INDEX
 ];
 
+// District RCD codes for non-Cook districts that we want to include
+var GLENBARD_DISTRICT_RCD = '190220870';
+var HINSDALE_DISTRICT_RCD = '190220860';
+var DUPAGE_DISTRICT_RCD = '190220880';
+var LARAWAY_DISTRICT_RCD = '56099070C';
+
+// DISTRICT codes non-Cook schools we want to include, including private schools
+var NON_COOK_RCD_CODES = new Set([
+  GLENBARD_DISTRICT_RCD,
+  HINSDALE_DISTRICT_RCD,
+  DUPAGE_DISTRICT_RCD,
+  LARAWAY_DISTRICT_RCD
+]);
+
 var INDEX_TO_LETTER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').reduce(function(lookup, letter, i) {
   lookup[i] = letter;
   return lookup;
 }, {});
 
 var filterRow = function(worksheet, i) {
+  var cell;
+
+  // Include header row
   if (i == 0) {
     return true;
   }
 
-  var countyNameRef = INDEX_TO_LETTER[COUNTY_NAME_COLUMN_INDEX] + (i + 1);
-  var cell = worksheet[countyNameRef];
-  if (!cell || cell.v != 'Cook') {
-    return false;
-  }
-
+  // Only include school records
   var recTypeRef = INDEX_TO_LETTER[REC_TYPE_COLUMN_INDEX] + (i + 1);
   cell = worksheet[recTypeRef];
-  if (!cell || cell.v != 'Sch') {
+  if (!cell || (cell.v != 'Sch' && cell.v != 'Non Pub Sch')) {
     return false;
   }
 
-  return true;
+  // Include schools with an RCD code in our explicit list
+  var rcdCodeRef = INDEX_TO_LETTER[RCD_CODE_COLUMN_INDEX] + (i + 1);
+  cell = worksheet[rcdCodeRef];
+  if (cell && NON_COOK_RCD_CODES.has(cell.v)) {
+    return true;
+  }
+
+  // Include all schools in cook county
+  var countyNameRef = INDEX_TO_LETTER[COUNTY_NAME_COLUMN_INDEX] + (i + 1);
+  cell = worksheet[countyNameRef];
+  if (cell && cell.v == 'Cook') {
+    return true;
+  }
+
+  // Othwerwise, exclude the record
+  return false;
 };
 
 var inputPath = process.argv[2];
@@ -58,8 +85,9 @@ var workbook = xlsx.readFile(inputPath);
 var writer = stringify();
 writer.pipe(process.stdout);
 
+var sheetNumber = 0;
 workbook.SheetNames.forEach(function(sheetName) {
-  if (sheetName != WORKSHEET_NAME) {
+  if (!SOURCE_WOORKSHEETS.has(sheetName)) {
     return;
   }
 
@@ -71,13 +99,25 @@ workbook.SheetNames.forEach(function(sheetName) {
       continue;
     }
 
+    // Only include header rows on first sheet
+    if (i == 0 && sheetNumber != 0) {
+      continue;
+    }
+
     var row = OUTPUT_COLUMNS.map(function(j) {
       var cellRef = INDEX_TO_LETTER[j] + (i + 1);
       var cell = worksheet[cellRef];
-      return cell.v;
+      if (cell) {
+        return cell.v;
+      }
+      else {
+        return null;
+      } 
     });
     writer.write(row);
   }
+
+  sheetNumber += 1;
 });
 
 writer.end();
