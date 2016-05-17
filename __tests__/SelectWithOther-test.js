d5d21a033a884a3666d8fa787d5e062f663b3e0f
Geoff Hing <geoffhing@gmail.com>
2016-05-16 22:53:21 -0500

    Add select + "other" form component
    Add a form component that starts as a select but allows a user
    to input a value other than the ones in the select element via
    a text input if "Other" is selected from the select element.
    
    Addresses #13

---
+++ __tests__/SelectWithOther-test.js
@@ -0,0 +1,130 @@
+jest.unmock('../js/src/components/SelectWithOther');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import SelectWithOther from '../js/src/components/SelectWithOther';
+
+describe('SelectWithOther', () => {
+  const options = [
+    'Dating/Partner Violence',
+    'Dating/Partner Violence/Bullying',
+    'Prevention + Intervention',
+    'Sexual Health',
+    'Sexual Violence',
+    'Sexual Violence/Exploitation'
+  ];
+
+  it('renders initially with a select and no text input', () => {
+    const component = TestUtils.renderIntoDocument(
+      <SelectWithOther />
+    );
+
+    const componentNode = ReactDOM.findDOMNode(component);
+    expect(componentNode.childNodes.length).toEqual(1);
+    expect(componentNode.firstChild.nodeName.toLowerCase()).toEqual('select');
+  });
+
+  it('renders options based on a list of strings passed as an options prompt', () => {
+
+    const component = TestUtils.renderIntoDocument(
+      <SelectWithOther options={options} />
+    );
+
+    const componentNode = ReactDOM.findDOMNode(component);
+
+    expect(componentNode.firstChild.firstChild.textContent).toEqual("");
+    expect(componentNode.firstChild.firstChild.getAttribute('value')).toEqual("");
+    options.forEach((option, i) => {
+      let optionNode = componentNode.firstChild.childNodes[i+1];
+      expect(optionNode.textContent).toEqual(option);
+      expect(optionNode.getAttribute('value')).toEqual(option);
+    });
+  });
+
+  it('renders an "other" option', () => {
+    const component = TestUtils.renderIntoDocument(
+      <SelectWithOther options={options} />
+    );
+
+    const componentNode = ReactDOM.findDOMNode(component);
+
+    expect(componentNode.firstChild.lastChild.textContent).toEqual("Other");
+    expect(componentNode.firstChild.lastChild.getAttribute('value')).toEqual("__other__");
+  });
+
+  it('renders a text input when you select "other"', () => {
+    const component = TestUtils.renderIntoDocument(
+      <SelectWithOther options={options} />
+    );
+
+    const componentNode = ReactDOM.findDOMNode(component);
+    const selectNode = componentNode.firstChild;
+
+    TestUtils.Simulate.change(selectNode, {
+      target: {
+        value: '__other__'
+      }
+    });
+
+    const inputNode = componentNode.lastChild;
+
+    expect(componentNode.childNodes.length).toEqual(2);
+    expect(inputNode.nodeName.toLowerCase()).toEqual('input');
+    expect(inputNode.getAttribute('type')).toEqual('text');
+  });
+
+  it('renders a text input with the specified value when not one of the options', () => {
+    const value = "Some other program";
+    const component = TestUtils.renderIntoDocument(
+      <SelectWithOther options={options} value={value} />
+    );
+    const componentNode = ReactDOM.findDOMNode(component);
+    const inputNode = componentNode.lastChild;
+    expect(componentNode.childNodes.length).toEqual(2);
+    expect(inputNode.nodeName.toLowerCase()).toEqual('input');
+    expect(inputNode.getAttribute('type')).toEqual('text');
+    expect(inputNode.getAttribute('value')).toEqual(value);
+  });
+
+  it('calls an onChange prop when the select changes', () => {
+    const value = "Sexual Health";
+    const newValue = "Sexual Violence";
+    const mockOnChange = jest.genMockFunction();
+    const component = TestUtils.renderIntoDocument(
+      <SelectWithOther options={options} value={value} onChange={mockOnChange} />
+    );
+    const componentNode = ReactDOM.findDOMNode(component);
+    const selectNode = componentNode.firstChild;
+    TestUtils.Simulate.change(selectNode, {
+      target: {
+        value: newValue
+      }
+    });
+    expect(mockOnChange.mock.calls.length).toEqual(1);
+    expect(mockOnChange.mock.calls[0][0]).toEqual(newValue);
+  });
+
+  it('calls an onChange prop when the other input changes', () => {
+    const value = "Sexual Health";
+    const newValue = "Totally new value";
+    const mockOnChange = jest.genMockFunction();
+    const component = TestUtils.renderIntoDocument(
+      <SelectWithOther options={options} value={value} onChange={mockOnChange} />
+    );
+    const componentNode = ReactDOM.findDOMNode(component);
+    const selectNode = componentNode.firstChild;
+    TestUtils.Simulate.change(selectNode, {
+      target: {
+        value: '__other__'
+      }
+    });
+    const inputNode = componentNode.lastChild;
+    TestUtils.Simulate.change(inputNode, {
+      target: {
+        value: newValue
+      }
+    });
+    expect(mockOnChange.mock.calls[mockOnChange.mock.calls.length-1][0]).toEqual(newValue);
+  });
+});
---
+++ js/src/components/SelectWithOther.js
@@ -0,0 +1,109 @@
+import React from 'react'; 
+
+/**
+ * React component that renders a select control that adds a text input
+ * allowing the user to enter an "Other" option.
+ */
+const SelectOther = React.createClass({
+  getDefaultProps: function() {
+    return {
+      options: [],
+      onChange: function(value) {} 
+    };
+  },
+
+  getInitialSelectValue: function() {
+    if (!this.props.value) {
+      return '';
+    } 
+
+    let i;
+    for (i = 0; i < this.props.options.length; i++) {
+      if (this.props.options[i] == this.props.value) {
+        return this.props.value;
+      }
+    }
+
+    return '__other__';
+  },
+
+  getInitialInputValue: function() {
+    if (!this.props.value) {
+      return '';
+    }
+
+    let i;
+    for (i = 0; i < this.props.options.length; i++) {
+      if (this.props.options[i] == this.props.value) {
+        return ''; 
+      }
+    }
+
+    return this.props.value;
+  },
+
+  getInitialState: function() {
+    return {
+      selectValue: this.getInitialSelectValue(),
+      inputValue: this.getInitialInputValue()
+    };
+  },
+
+  getInput: function() {
+    if (this.state.selectValue == '__other__') {
+      return <input type="text" value={this.state.inputValue} onChange={this.handleChangeInput} />;
+    }
+
+    return false;
+  },
+
+  render: function() {
+    const options = [
+      <option value="" key=""></option>
+    ]
+    .concat(this.props.options.map(option => {
+        return <option value={option} key={option}>{option}</option>;
+    }))
+    .concat([
+      <option value="__other__" key="__other__">Other</option>
+    ]);
+
+    const input = this.getInput();
+
+    return (
+      <div>
+        <select value={this.state.selectValue} onChange={this.handleChangeSelect}>
+          {options}
+        </select>
+        {input}
+      </div>
+    );
+  },
+
+  handleChangeSelect: function(e) {
+    this.setState({
+      selectValue: e.target.value 
+    }, () => {
+      this.props.onChange(this.getValue());
+    });
+  },
+
+  handleChangeInput: function(e) {
+    this.setState({
+      inputValue: e.target.value
+    }, () => {
+      this.props.onChange(this.getValue());
+    });
+  },
+
+  getValue: function() {
+    if (this.state.selectValue == '__other__') {
+      return this.state.inputValue;
+    }
+    else {
+      return this.state.selectValue;
+    }
+  }
+});
+
+export default SelectOther;
