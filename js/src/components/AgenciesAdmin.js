5b6a6ca1f436436f0f84f166e3bce3754a815cee
Geoff Hing <geoffhing@gmail.com>
2017-06-07 18:57:05 -0500

    Lift up state when editing agencies
    Keep state for the actively edited or created agency in AgencyAdmin
    rather than AgencyAdminForm.
    
    This allows the agency's fields to be visible whether clicking on a link
    from the list view or visiting `/admin/agencies/:slug` directly.
    
    For more on lifting state up, see this page in the React documentation:
    https://facebook.github.io/react/docs/lifting-state-up.html.
    
    Addresses #71

--- js/src/components/AgenciesAdmin.js
+++ js/src/components/AgenciesAdmin.js
@@ -5,19 +5,33 @@ import LearningCollaborativeActions from '../actions/LearningCollaborativeAction
 import AgencyStore from '../stores/AgencyStore';
 import AdminTable from './AdminTable';
 
-
+/**
+ * Parent view for listing, editing and creating agencies.
+ */
 class AgenciesAdmin extends React.Component {
   constructor(props) {
     super(props);
+
+    const slug = props.params && props.params.slug ? props.params.slug : undefined;
+
     this.state = {
-      agencies: AgencyStore.getAll()
+      agencies: AgencyStore.getAll(),
+      // Agency being created or edited
+      activeAgency: AgenciesAdmin.getActiveAgency(slug),
     };
 
+    // Bind class methods to this instance so `this` refers to the class
+    // instance, even when the method is called in an event handler.
     this._onAgenciesChange = this._onAgenciesChange.bind(this);
-  }
-
-  getEditUrl(agency) {
-    return '/admin/agencies/' + agency.properties.slug;
+    this.handleChangeSlug = this.handleChangeSlug.bind(this);
+    this.handleChangeAgency = this.handleChangeAgency.bind(this);
+    this.handleChangeCatchmentArea = this.handleChangeCatchmentArea.bind(this);
+    this.handleChangeProgramType = this.handleChangeProgramType.bind(this);
+    this.handleChangeOfficeLocation = this.handleChangeOfficeLocation.bind(this);
+    this.handleChangeLat = this.handleChangeLat.bind(this);
+    this.handleChangeLng = this.handleChangeLng.bind(this);
+    this.handleChangeMarkerColor = this.handleChangeMarkerColor.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   render() {
@@ -32,6 +46,8 @@ class AgenciesAdmin extends React.Component {
     );
   }
 
+  // Component lifecycle methods
+
   componentDidMount() {
     AgencyStore.addChangeListener(this._onAgenciesChange);
   }
@@ -40,18 +56,48 @@ class AgenciesAdmin extends React.Component {
     AgencyStore.removeChangeListener(this._onAgenciesChange);
   }
 
-  _onAgenciesChange(method) {
-    this.setState({
-      agencies: AgencyStore.getAll()
-    });
 
-    if (method == 'update' || method == 'create') {
-      this.props.router.push('/admin/agencies');
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.params.slug) {
+      this.setState({
+        activeAgency: AgenciesAdmin.getActiveAgency(nextProps.params.slug)
+      });
     }
   }
 
+  /**
+   * Returns the URL for the edit view of a particular agency.
+   */
+  getEditUrl(agency) {
+    return '/admin/agencies/' + agency.properties.slug;
+  }
+
+  /**
+   * Returns an agency object to be edited.
+   *
+   * It also flattens out the GeoJSON object returned by AgencyStore so the
+   * return object is just a flat JavaScript object.
+   */
+  static getActiveAgency(slug) {
+    const agency = AgencyStore.getAgency(slug);
+    if (!agency) {
+      return null;
+    }
+
+    const activeAgency = Object.assign({}, agency.properties);
+    const coords = agency.geometry.coordinates;
+    activeAgency.lat = coords[1];
+    activeAgency.lng = coords[0];
+    return activeAgency;
+  }
+
+  /**
+   * Returns an iterable of child components that have needed props set.
+   */
   _getChildren(props) {
     if (!props.children) {
+      // No child components set by router.
+      // Return a list of agencies.
       return (
         <div>
           <div className="admin-buttons">
@@ -64,13 +110,26 @@ class AgenciesAdmin extends React.Component {
       );
     }
 
-    const agencyProps = {
-      handleCreate: LearningCollaborativeActions.createAgency,
-      handleUpdate: LearningCollaborativeActions.updateAgency
+    // There are child components set by the router.  This will be the add or
+    // edit form.
+    // Set form input change event handler props to methods of this class.
+    let agencyProps = {
+      onSubmit: this.handleSubmit,
+      onChangeAgency: this.handleChangeAgency,
+      onChangeSlug: this.handleChangeSlug,
+      onChangeCatchmentArea: this.handleChangeCatchmentArea,
+      onChangeProgramType: this.handleChangeProgramType,
+      onChangeOfficeLocation: this.handleChangeOfficeLocation,
+      onChangeLat: this.handleChangeLat,
+      onChangeLng: this.handleChangeLng,
+      onChangeMarkerColor: this.handleChangeMarkerColor,
+      slugDisabled: this.props.params.slug ? true : false,
     };
 
-    if (props.params.slug && this._hasAgencies()) {
-      agencyProps.agency = AgencyStore.getAgency(props.params.slug);
+    if (this.state.activeAgency) {
+      // We're in the process of creating or editing an agency.  Set the props
+      // that will be used for the form input values.
+      agencyProps = Object.assign(agencyProps, this.state.activeAgency);
     }
 
     return React.Children.map(props.children, child => React.cloneElement(child, agencyProps));
@@ -78,6 +137,119 @@ class AgenciesAdmin extends React.Component {
 
   _hasAgencies() {
     return (this.state.agencies && this.state.agencies.length > 0);
+  }
+
+  /**
+   * Updates state to reflect the values in the add/edit form.
+   */
+  updateActiveAgency(props) {
+    let activeAgency = {
+      slug: '',
+      agency: '',
+      catchment_area: '',
+      program_type: '',
+      office_location: '',
+      lat: '',
+      lng: '',
+      marker_color: '#000000'
+    };
+
+    if (this.state.activeAgency) {
+      activeAgency = Object.assign(activeAgency, this.state.activeAgency);
+    }
+
+    this.setState({
+      activeAgency: Object.assign(activeAgency, props),
+    });
+  }
+
+  // Event handlers
+
+  _onAgenciesChange(method) {
+    const newState = {
+      agencies: AgencyStore.getAll()
+    };
+    let callback;
+
+    if (method == 'update' || method == 'create') {
+      newState.activeAgency = null;
+      callback = () => {
+        this.props.router.push('/admin/agencies');
+      };
+    }
+    else if (this.props.params.slug) {
+      newState.activeAgency = AgenciesAdmin.getActiveAgency(this.props.params.slug);
+    }
+
+    this.setState(newState, callback);
+  }
+
+  handleSubmit() {
+    const handleSubmit = this.props.params.slug ? LearningCollaborativeActions.updateAgency : LearningCollaborativeActions.createAgency;
+    const activeAgency = this.state.activeAgency;
+
+    handleSubmit(
+      activeAgency.slug,
+      activeAgency.agency,
+      activeAgency.catchment_area,
+      activeAgency.program_type,
+      activeAgency.office_location,
+      activeAgency.lat,
+      activeAgency.lng,
+      activeAgency.marker_color
+    );
+
+    this.setState({
+      activeAgency: null,
+    });
+  }
+
+  handleChangeSlug(value) {
+    this.updateActiveAgency({
+      slug: value
+    });
+  }
+
+  handleChangeAgency(value) {
+    this.updateActiveAgency({
+      agency: value
+    });
+  }
+
+  handleChangeCatchmentArea(value) {
+    this.updateActiveAgency({
+      catchment_area: value
+    });
+  }
+
+  handleChangeProgramType(value) {
+    this.updateActiveAgency({
+      program_type: value
+    });
+  }
+
+  handleChangeOfficeLocation(value) {
+    this.updateActiveAgency({
+      office_location: value
+    });
+  }
+
+  handleChangeLat(value) {
+    this.updateActiveAgency({
+      lat: value
+    });
+  }
+
+  handleChangeLng(value) {
+    this.updateActiveAgency({
+      lng: value
+    });
+  }
+
+  handleChangeMarkerColor(value) {
+    this.updateActiveAgency({
+      marker_color: value
+    });
   }
 }
 
--- js/src/components/AgenciesAdminForm.js
+++ js/src/components/AgenciesAdminForm.js
@@ -5,33 +5,6 @@ class AgenciesAdminForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      slug: '',
-      agency: '',
-      catchment_area: '',
-      program_type: '',
-      office_location: '',
-      lat: '',
-      lng: '',
-      marker_color: '#000000'
-    };
-
-    if (props.agency) {
-      const coords = props.agency.geometry.coordinates;
-      const agencyProps = props.agency.properties;
-
-      this.state.slug = agencyProps.slug;
-      this.state.slugDisabled = true;
-      this.state.agency = agencyProps.agency;
-      this.state.catchment_area = agencyProps.catchment_area;
-      this.state.program_type = agencyProps.program_type;
-      this.state.office_location = agencyProps.office_location;
-      this.state.marker_color = agencyProps.marker_color;
-
-      this.state.lat = coords[1];
-      this.state.lng = coords[0];
-    }
-
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChangeAgency = this.handleChangeAgency.bind(this);
     this.handleChangeSlug = this.handleChangeSlug.bind(this);
@@ -49,42 +22,42 @@ class AgenciesAdminForm extends React.Component {
       <form className="agencies-admin-form" onSubmit={this.handleSubmit}>
         <fieldset className="form-group">
           <label htmlFor="slug">Slug</label>
-          <input type="text" id="slug" value={this.state.slug} onChange={this.handleChangeSlug} ref="slug" className="form-control" required="required" disabled={this.state.slugDisabled} />
+          <input type="text" id="slug" value={this.props.slug} onChange={this.handleChangeSlug} className="form-control" required="required" disabled={this.props.slugDisabled} />
         </fieldset>
 
         <fieldset className="form-group">
           <label htmlFor="agency">Agency name</label>
-          <input type="text" id="agency" value={this.state.agency} onChange={this.handleChangeAgency} ref="agency" className="form-control" required="required" />
+          <input type="text" id="agency" value={this.props.agency} onChange={this.handleChangeAgency} className="form-control" required="required" />
         </fieldset>
 
         <fieldset className="form-group">
           <label htmlFor="catchment_area">Catchment area</label>
-          <input type="text" id="catchment_area" value={this.state.catchment_area} onChange={this.handleChangeCatchmentArea} ref="catchment_area" className="form-control" />
+          <input type="text" id="catchment_area" value={this.props.catchment_area} onChange={this.handleChangeCatchmentArea} className="form-control" />
         </fieldset>
 
         <fieldset className="form-group">
           <label htmlFor="program_type">Program type</label>
-          <input type="text" id="program_type" value={this.state.program_type} onChange={this.handleChangeProgramType} ref="program_type" className="form-control" />
+          <input type="text" id="program_type" value={this.props.program_type} onChange={this.handleChangeProgramType} className="form-control" />
         </fieldset>
 
         <fieldset className="form-group">
           <label htmlFor="office_location">Office location</label>
-          <textarea id="office_location" value={this.state.office_location} onChange={this.handleChangeOfficeLocation} ref="office_location" className="form-control" />
+          <textarea id="office_location" value={this.props.office_location} onChange={this.handleChangeOfficeLocation} className="form-control" />
         </fieldset>
 
         <fieldset className="form-group">
           <label htmlFor="lat">Latitude</label>
-          <input type="text" id="lat" value={this.state.lat} onChange={this.handleChangeLat} ref="lat" className="form-control" />
+          <input type="text" id="lat" value={this.props.lat} onChange={this.handleChangeLat} className="form-control" />
         </fieldset>
 
         <fieldset className="form-group">
           <label htmlFor="lng">Longitude</label>
-          <input type="text" id="lng" value={this.state.lng} onChange={this.handleChangeLng} ref="lng" className="form-control" />
+          <input type="text" id="lng" value={this.props.lng} onChange={this.handleChangeLng} className="form-control" />
         </fieldset>
 
         <fieldset className="form-group">
           <label htmlFor="marker_color">Marker color</label>
-          <input type="color" id="marker_color" value={this.state.marker_color} onChange={this.handleChangeMarkerColor} ref="marker_color" className="form-control" />
+          <input type="color" id="marker_color" value={this.props.marker_color} onChange={this.handleChangeMarkerColor} className="form-control" />
         </fieldset>
 
         <button className="btn btn-primary" type="submit">Submit</button>
@@ -94,79 +67,52 @@ class AgenciesAdminForm extends React.Component {
 
   handleSubmit(evt) {
     evt.preventDefault();
-
-    const handleSubmit = this.props.agency ? this.props.handleUpdate : this.props.handleCreate;
-
-    handleSubmit(
-      this.state.slug,
-      this.state.agency,
-      this.state.catchment_area,
-      this.state.program_type,
-      this.state.office_location,
-      this.state.lat,
-      this.state.lng,
-      this.state.marker_color
-    );
-
-    this.setState({
-      slug: '',
-      agency: '',
-      catchment_area: '',
-      program_type: '',
-      office_location: '',
-      lat: '',
-      lng: '',
-      marker_color: '#000000'
-    });
+    this.props.onSubmit();
   }
 
   handleChangeAgency(evt) {
-    this.setState({
-      agency: evt.target.value
-    });
+    this.props.onChangeAgency(evt.target.value);
   }
 
   handleChangeSlug(evt) {
-    this.setState({
-      slug: evt.target.value
-    });
+    this.props.onChangeSlug(evt.target.value);
   }
 
   handleChangeCatchmentArea(evt) {
-    this.setState({
-      catchment_area: evt.target.value
-    });
+    this.props.onChangeCatchmentArea(evt.target.value);
   }
 
   handleChangeProgramType(evt) {
-    this.setState({
-      program_type: evt.target.value
-    });
+    this.props.onChangeProgramType(evt.target.value);
   }
 
   handleChangeOfficeLocation(evt) {
-    this.setState({
-      office_location: evt.target.value
-    });
+    this.props.onChangeOfficeLocation(evt.target.value);
   }
 
   handleChangeLat(evt) {
-    this.setState({
-      lat: evt.target.value
-    });
+    this.props.onChangeLat(evt.target.value);
   }
 
   handleChangeLng(evt) {
-    this.setState({
-      lng: evt.target.value
-    });
+    this.props.onChangeLng(evt.target.value);
   }
 
   handleChangeMarkerColor(evt) {
-    this.setState({
-      marker_color: evt.target.value
-    });
+    this.props.onChangeMarkerColor(evt.target.value);
   }
 }
+
+AgenciesAdminForm.defaultProps = {
+  slug: "",
+  slugDisabled: false,
+  agency: "",
+  catchment_area: "",
+  program_type: "",
+  office_location: "",
+  lat: "",
+  lng: "",
+  marker_color: "#000000",
+};
 
 export default AgenciesAdminForm;
